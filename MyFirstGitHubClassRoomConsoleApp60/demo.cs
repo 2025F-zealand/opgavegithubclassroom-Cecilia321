f1bc8d870c99f3a447a6fd933a3090eb61133d43
Cecilia Liv Starck <cecilia-liv@hotmail.com>
2025-02-06 12:42:40 +0100

    Ny klasse med proporties

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class demo
+    {
+        private string name { get; set; }
+        private int number { get; set; }
+
+    }
+}
